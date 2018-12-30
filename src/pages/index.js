d1faec9fdbb5c435d3f9d3f5fd3c1efa69868f1a
Alexander Zonov <blogknot@gmail.com>
2018-12-31 02:52:18 +0300

    styles organized a little bit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import Layout from '../components/layout'
 
+const backgroundStyles = {
+  height: '100%',
+  background: `url(images/dust_scratches.png) repeat`,
+  textAlign: 'center'
+};
+
+const textStyles = {
+  textAlign: 'left',
+  display: 'inline-block',
+  background: 'none',
+  marginTop: '100px'
+}
+
 const IndexPage = () => (
   <Layout>
-    <div style={{
-      height: '100%',
-      background: `url(images/dust_scratches.png) repeat`,
-      'text-align': 'center'
-    }}>
-      <pre style={{'text-align': 'left', 'display': 'inline-block', 'background': 'none', 'margin-top': '100px'}}>
+    <div style={backgroundStyles}>
+      <pre style={textStyles}>
         <code>
           {`
   while(vacations) {
