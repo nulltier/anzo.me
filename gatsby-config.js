3d414b75d676d07fff5137783f91019f1dcace60
Alexander Zonov <anzome@outlook.com>
2019-12-08 14:24:07 +0300

    yandex's and google's counters added, just in case, to have experience with them

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,23 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `anzome is me`,
+    description: `nite attempt to start publicly available web page`,
     author: `@fort_wrong`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `images`,
-    //     path: `${__dirname}/src/images`,
-    //   },
-    // },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `anzo.me`,
-        short_name: `blog`,
+        short_name: `playground for me`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
-  ],
+        display: `minimal-ui`
+      }
+    }
+  ]
 }
--- src/components/stats-counters.js
+++ src/components/stats-counters.js
@@ -5,30 +5,26 @@ export default () => (
     <script type="text/javascript" dangerouslySetInnerHTML={{__html: `
       (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
       m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+      
       (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-      ym(51772067, "init", {
-        id:51772067,
+      ym(56576185, "init", {
         clickmap:true,
         trackLinks:true,
-        accurateTrackBounce:true
+        accurateTrackBounce:true,
+        webvisor:true
       });
     `}}/>
-    <noscript>
-      <div>
-        <img src="https://mc.yandex.ru/watch/51772067" style={{"position": 'absolute', "left": '-9999px;'}} alt="" />
-      </div>
-    </noscript>
+    <noscript><div><img src="https://mc.yandex.ru/watch/56576185" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 
-    {/*=========================================*/}
-
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131435638-1"/>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-36942724-2"></script>
     <script dangerouslySetInnerHTML={{__html: `
-    window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments)}
       gtag('js', new Date());
 
-      gtag('config', 'UA-131435638-1');
+      gtag('config', 'UA-36942724-2');
     `}}/>
+
   </React.Fragment>
 );
